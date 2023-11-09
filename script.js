ef94fc2ca620331df545dac8e74910f336b5fc8f
MaximoJFS <149447549+MaximoJFS@users.noreply.github.com>
2023-11-08 23:25:31 -0500

    Add files via upload
    Agregue un Archivo ZIP para que pueda ver todos los archivos.

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+  // JavaScript para cambiar el fondo al hacer clic en el botón
+  document.getElementById("cambiarFondoButton").addEventListener("click", function () {
+      // Cambia el fondo de la página actual al color deseado
+      document.body.style.backgroundColor = "#ff6600";
+  });
+</script>
+
+
+
+
+
+
